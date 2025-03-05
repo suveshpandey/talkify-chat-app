c99880f336c96d4f3da27adca527c734212d8a4e
Suvesh Pandey <jpsuvesh29@gmail.com>
2025-03-05 16:24:09 +0530

    initial commit - project completed

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    // baseURL: "http://localhost:3000/api",
-    baseURL: "https://talkify-backend-a3cu.onrender.com/api", // Updated backend URL
+    baseURL: "http://localhost:3000/api",
     withCredentials: true
 })
--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -4,9 +4,7 @@ import { AxiosError } from "axios";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-// const BASE_URL = "http://localhost:3000";
-const BASE_URL = "https://talkify-backend-a3cu.onrender.com"; // Updated backend URL
-
+const BASE_URL = "http://localhost:3000";
 interface AuthState {
     authUser: any | null,
     isSigninup: boolean,
